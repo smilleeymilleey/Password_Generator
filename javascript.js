f83c7448b69b7d24fe6e6e66a3773e22b8f9733f
Caitlyn <caitlynmiley.cm@gmail.com>
2021-03-17 11:51:22 -0500

    adding homework template to get started

--- javascript.js
+++ javascript.js
@@ -1,10 +1,38 @@
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+// this is my code and above is what the homework gave 
+
+
+
 function getRandomPassword() {
     let characters = ["a","b","c","d","e","f","g","h","i"]
 
     console.log(Math.floor(Math.random() * characters.length))
 
     for (let i = 0 < characters.length; i++)      
-        const newCharacter = characters[i] + "";
+        let newCharacter = characters[i] + "";
     
    
 };
