667892f13d80047d6322fc90a7d3a5e87b0e62aa
Caitlyn <caitlynmiley.cm@gmail.com>
2021-03-17 08:38:26 -0500

    attempted first part of password generator. created the array, random character generator, and converted it to a string

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function getRandomPassword() {
+    let characters = ["a","b","c","d","e","f","g","h","i"]
+
+    console.log(Math.floor(Math.random() * characters.length))
+
+    for (let i = 0 < characters.length; i++)      
+        const newCharacter = characters[i] + "";
+    
+   
+};
+
+getRandomPassword();
