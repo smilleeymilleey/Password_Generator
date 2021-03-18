746507373b6996cc5d1e9a7a8ef83c3e8d2f2c0b
Caitlyn <caitlynmiley.cm@gmail.com>
2021-03-17 19:56:44 -0500

    added an alert that takes the length and only accepts numbers

--- javascript.js
+++ javascript.js
@@ -1,40 +1,52 @@
+// when button is clicked a prompt shows password criteria
+// prompt for length of password -between 8 and 120-
+// prompt for character type -lowercase, uppercase, numbers, and special characters-
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// when prompts are answered the password is generated 
+// then the password is displayed in an alert or written to the page 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword () {
+  
+   let passwordLength= Number(window.prompt("How many characters do you want your password to be?(must be between 8-120 characters long)", ""));
+   let lowerCaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+   let upperCaseCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+   let numberCharacters = [1,2,3,4,5,6,7,8,9,0];
+   let specialCharacters = ["!","@","#","$","%","&","?"]
 
-  passwordText.value = password;
+   let allCharacters = lowerCaseCharacters.concat(upperCaseCharacters, numberCharacters, specialCharacters);
 
-}
+   var allRandomCharacters = Math.floor(Math.random() * allCharacters.length )
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+   passwordLength = parseInt(passwordLength);
 
+   if (isNaN(passwordLength)) {
+     alert("Invalid Input");
+   }
 
-
-
-
-
-
-
-
-// this is my code and above is what the homework gave 
-
-
-
-function getRandomPassword() {
-    let characters = ["a","b","c","d","e","f","g","h","i"]
-
-    console.log(Math.floor(Math.random() * characters.length))
-
-    for (let i = 0 < characters.length; i++)      
-        let newCharacter = characters[i] + "";
-    
+   else {
+     alert("The number of characters is " + passwordLength + ".")
+   }
    
-};
 
-getRandomPassword();
+  //  setting password length 
+   if (passwordLength <= 7) {
+     console.log("your password is too short")
+   } else if (passwordLength >= 120) {
+     console.log("your password is too long")
+   } else {
+     console.log("your password length is set")
+   }
+  
+   for (let i = 0; i < passwordLength.length; i++) {
+     const element = array[i];
+     
+   }
+  //  setting password character type 
+
+
+
+  }
+
+
+
+generatePassword();
